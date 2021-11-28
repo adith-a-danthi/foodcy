47d78b6a7e5bcf4606c5276dbe125f2c17602314
Adith A Danthi <adith.danthi@gmail.com>
2021-11-28 22:24:07 +0530

    ✨ Added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="content">
+      <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+html {
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  line-height: 1.4;
+}
+
+*,
+*::before,
+*::after {
+  font-family: 'Mulish', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+}
+</style>
