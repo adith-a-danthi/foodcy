42303b994e725d88547e41c196b7f507b3cdfbf3
Adith A Danthi <adith.danthi@gmail.com>
2021-11-29 21:07:42 +0530

    ✨ Added toast component

---
+++ components/AppToast.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="toast">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.toast {
+  background: #a8060e;
+  color: white;
+  border-radius: 0.5rem;
+  padding: 0.75rem 1.5rem;
+  text-align: center;
+
+  a {
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
